4c12c047edabad1039835130ef19907d0eda94c1
Alec Dewulf <56365692+AWD81@users.noreply.github.com>
2019-12-17 11:32:38 -0500

    Create J1.py

---
+++ CCC2014/J1.py
@@ -0,0 +1,16 @@
+# Alec Dewulf
+# J1 2014
+
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+
+if a1 + a2 + a3 == 180:
+    if a1 == 60 and a2 == 60 and a3 == 60:
+        print("Equilateral")
+    elif a1 == a2 or a1 == a3 or a2 == a3:
+        print("Isosceles")
+    elif a1 != a2 and a1 != a3 and a2 != a3:
+        print("Scalene")
+else:
+    print("Error")
