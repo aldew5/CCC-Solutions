43c092fe5f0e672a520e789dea40dcd6583396d5
Alec Dewulf <56365692+AWD81@users.noreply.github.com>
2019-12-25 18:51:03 -0500

    Update J1.py

--- CCC2017/J1.py
+++ CCC2017/J1.py
@@ -1,1 +1,15 @@
+# "Quadrant Selection" J1 2017
+# Alec Dewulf
+
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print("1")
+elif x < 0 and y > 0:
+    print("2")
+elif x < 0 and y < 0:
+    print("3")
+elif x > 0 and y < 0:
+    print("4")
 
