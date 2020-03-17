fea9b9ba65923743f02025f483cd2bc1dc383883
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-03-17 19:39:48 -0400

    Update J1.java

--- 2014/J1.java
+++ 2014/J1.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		
-    // get input
+    		// get input
 		int a1 = scanner.nextInt();
 		int a2 = scanner.nextInt();
 		int a3 = scanner.nextInt();
 		
-    //check conditions
+    		//check conditions
 		if ((a1 + a2 + a3) != 180) System.out.println("Error");
 		else if (a1 == a2 && a2 == a3) System.out.println("Equilateral");
 		else if (a1 == a2| a2 == a3 | a1 == a3) System.out.println("Isosceles");
